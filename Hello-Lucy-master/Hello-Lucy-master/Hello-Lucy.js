d854f336265381d2654467640e15b18c6c92196d
pritesh2602 <pritesh2602@users.noreply.github.com>
2021-01-30 18:04:14 +0530

    Add files via upload

---
+++ Hello-Lucy-master/Hello-Lucy-master/Hello-Lucy.js
@@ -0,0 +1,293 @@
+"use strict";
+
+var importedSentences;                              /** Prepare variable to be populated from import. */
+function readTextFile(file, callback) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4 && rawFile.status == "200") {
+			callback(rawFile.responseText);
+		}
+	};
+	rawFile.send(null);
+}
+const sfn=document.currentScript.src.substring(7,document.currentScript.src.lastIndexOf(document.currentScript.src.includes('//')?'//':'/'))
+const localPath= sfn.substring(sfn.indexOf('modules'))
+const ModuleName=localPath.substring(localPath.lastIndexOf("/")+1)
+//console.log("localPath="+localPath+" modulename="+ModuleName);
+
+readTextFile(localPath+"/sentences.json", function(text){
+	var tempImport = JSON.parse(text);
+	importedSentences = tempImport;
+	// console.log("SENTENCE: "+importedSentences);
+});
+
+////////////////////////////////////////////////////
+
+Module.register(ModuleName, {
+
+	icon: "fa-microphone-slash",
+	pulsing: true,
+	help: false,
+	timeout: null,
+	lucy: {
+		mode: "Say, 'Hello Lucy'",
+		sentences: []
+	},
+
+	modules: [],
+	previouslyHidden: [],
+
+	defaults: {
+		timeout: 15,                                // time listening for a command/sentence when mic pulsing
+		defaultOnStartup: "Hello-Lucy",
+		keyword: "HELLO LUCY",                      // keyword to activate listening for a command/sentence
+		standByMethod: "DPMS",                      // 'DPMS' = anything else than RPi or 'PI'
+		sounds: ["a.mp3",  "b.mp3",  "c.mp3" , "d.mp3"], // welcome sounds at startup randomly chosen
+		startHideAll: false,                        // if true, all modules start as hidden
+		microphone: "default",                      // 
+		speed: 1000,                                // transition speed between show/no-show/show in milliseconds
+	    	pageOneModules: ["Hello-Lucy"],         // default modules to show on page one/startup
+		    pageTwoModules: [],                         // modules to show on page two
+    		pageThreeModules: [],                       // modules to show on page two
+    		pageFourModules: [],                        // modules to show on page two
+    		pageFiveModules: [],                        // modules to show on page two
+    		pageSixModules: [],                         // modules to show on page two
+    		pageSevenModules: [],                       // modules to show on page two
+    		pageEightModules: [],                       // modules to show on page two
+    		pageNineModules: [],                        // modules to show on page two
+    		pageTenModules: [],                         // modules to show on page two
+		    greetingSounds: [ "a.mp3",  "b.mp3",  "c.mp3" , "d.mp3"], // randomized greeting sounds
+				confirmationSound: "ding.mp3",               // when command is accepted. use your own or default
+				debug: false,                               // get debug information in console
+	},
+
+	poweredOff: false,
+
+	start() {
+		var combinedSentences = importedSentences.concat(this.lucy.sentences);
+		this.lucy.sentences = combinedSentences;
+		//Log.info(`Starting module: ${this.name}`);
+		this.mode = this.translate("INIT");
+		this.modules.push(this.lucy);
+		//Log.info(`${this.name} is waiting for lucy command registrations.`);
+	},
+
+	getStyles() {
+		return ["font-awesome.css", "Hello-Lucy.css"];
+	},
+
+	getTranslations() {
+		return {
+			en: "translations/en.json",
+			de: "translations/de.json",
+			id: "translations/id.json"
+		};
+	},
+
+  // audible confirmation sound that Lucy understood the command
+	playConfirmationSound() {
+		var audio = new Audio(localPath+"/sounds/" + this.config.confirmationSound);
+		audio.play();
+	},
+
+  // randomized greeting sounds
+	playGreetingSound() {
+		var random_file = this.config.sounds[Math.floor(Math.random() * this.config.sounds.length)];
+		var audio = new Audio(localPath+"/sounds/"+random_file);
+		audio.play();
+	},
+
+	getDom() {
+		//Log.log("lucy entered getDom")
+		const wrapper = document.createElement("div");
+		const lucy = document.createElement("div");
+		lucy.classList.add("small", "align-left");
+
+		const modeSpan = document.createElement("span");
+		const icon = document.createElement("i");
+		icon.classList.add("fa", this.icon, "icon");
+		if (this.pulsing) {
+			icon.classList.add("pulse");
+			modeSpan.innerHTML = "Command Please:"
+		}
+		else
+			modeSpan.innerHTML = this.mode;
+		lucy.appendChild(icon);
+		lucy.appendChild(modeSpan);
+
+		if (this.config.debug) {
+			const debug = document.createElement("div");
+			debug.innerHTML = this.debugInformation;
+			lucy.appendChild(debug);
+		}
+
+		const modules = document.querySelectorAll(".module");
+		for (let i = 0; i < modules.length; i += 1) {
+			if (!modules[i].classList.contains(this.name)) {
+				if (this.help) {
+					modules[i].classList.add(`${this.name}-blur`);
+				} else {
+					modules[i].classList.remove(`${this.name}-blur`);
+				}
+			}
+		}
+
+		if (this.help) {
+			lucy.classList.add(`${this.name}-blur`);
+			const modal = document.createElement("div");
+			modal.classList.add("modal");
+			this.appendHelp(modal);
+			wrapper.appendChild(modal);
+		}
+
+		wrapper.appendChild(lucy);
+
+		return wrapper;
+	},
+
+	notificationReceived(notification, payload, sender) {
+		//Log.log("lucy module notification received="+notification)
+		var self=this;
+		if (notification === "DOM_OBJECTS_CREATED") {
+			//Log.log("lucy module sending start")
+			this.sendSocketNotification("START", { config: this.config, modules: this.modules });
+		} else if (notification === "REGISTER_LUCY_MODULE") {
+			if (Object.prototype.hasOwnProperty.call(payload, "mode") && Object.prototype.hasOwnProperty.call(payload, "sentences")) {
+				this.modules.push(payload);
+		}
+		}
+       // randomly chosen startup greeting
+		if (notification === "DOM_OBJECTS_CREATED") {
+			this.playGreetingSound();
+		}
+
+		// @TheStigh to manage hide/show modules on startup
+		if (this.config.startHideAll) {
+			if (notification === "DOM_OBJECTS_CREATED") {
+				MM.getModules().enumerate((module) => {
+					module.hide(1000);
+				});
+			}
+		}
+
+		if (notification === "DOM_OBJECTS_CREATED"){
+			var showOnStart = MM.getModules().withClass(self.config.pageOneModules);
+			showOnStart.enumerate(function(module) {
+				var callback = function(){};
+				module.show(self.config.speed, callback);
+			});
+		}
+
+		if (notification === "DOM_OBJECTS_CREATED"){
+			var showOnStart = MM.getModules().withClass(self.config.defaultOnStartup);
+			showOnStart.enumerate(function(module) {
+				var callback = function(){};
+				module.show(self.config.speed, callback);
+			});
+		}
+
+		// to show page one on alert @TheStigh
+		if (notification === "SHOW_ALERT") {
+			var showOnStart = MM.getModules().withClass(self.config.pageOneModules);
+			showOnStart.enumerate(function(module) {
+				var callback = function(){};
+				module.show(self.config.speed, callback);
+			});
+		}
+ 	},
+
+	socketNotificationReceived(notification, payload) {
+		if (notification === "READY") {
+			this.icon = "fa-microphone";
+			this.mode = this.translate("NO_MODE")+ "'Hello Lucy'";
+			this.pulsing = false;
+
+		} else if (notification === "LISTENING") {
+			this.pulsing = true;
+
+		} else if (notification === "SLEEPING") {
+			this.pulsing = false;
+
+		} else if (notification === "ERROR") {
+			this.mode = notification;
+
+		} else if (notification === "LUCY") {
+			for (let i = 0; i < this.modules.length; i += 1) {
+				if (payload.mode === this.modules[i].mode) {
+					if (this.mode !== payload.mode) {
+						this.help = false;
+						this.sendNotification(`${notification}_MODE_CHANGED`, { old: this.mode, new: payload.mode });
+						this.mode = payload.mode;
+					}
+					if (this.mode !== "LUCY") {
+						this.sendNotification(`${notification}_${payload.mode}`, payload.sentence);
+					}
+					break;
+				}
+			}
+
+		} else if (notification === "HIDE_MODULES") {
+			this.playConfirmationSound();
+
+			MM.getModules().enumerate((module) => {
+				module.hide(1000);
+			});
+
+		} else if (notification === "SHOW_MODULES") {
+			this.playConfirmationSound();
+
+			MM.getModules().enumerate((module) => {
+				module.show(1000);
+			});
+
+		} else if (notification === "DEBUG") {
+			this.debugInformation = payload;
+
+		} else if (notification === "MODULE_STATUS") {
+			this.playConfirmationSound();
+
+			var hide = MM.getModules().withClass(payload.hide);
+			hide.enumerate(function(module) {
+				//Log.log("Hide "+ module.name);
+				var callback = function(){};
+				module.hide(self.config.speed, callback);
+			});
+
+			var show = MM.getModules().withClass(payload.show);
+			show.enumerate(function(module) {
+				//Log.log("Show "+ module.name);
+				var callback = function(){};
+				module.show(self.config.speed, callback);
+			});
+
+		} else if (notification === "MODULE_UPDATE") {
+			this.sendNotification(payload);
+			//console.log("sendNoti received :"+payload);
+
+		} else if (notification === "CLOSE_HELP") {
+			this.help = false;
+		} else if (notification === "OPEN_HELP") {
+			this.help = true;
+		}
+		this.updateDom(300);
+	},
+
+	appendHelp(appendTo) {
+		const listLabel = document.createElement("div");
+		listLabel.classList.add("medium");
+		// heading for help screen
+		listLabel.innerHTML = `${this.translate("Lucy Commands")}:`;
+		appendTo.appendChild(listLabel);
+
+		const list = document.createElement("ul");
+		for (let i = 0; i < this.lucy.sentences.length; i += 1) {
+			const item = document.createElement("li");
+			list.classList.add("xsmall");
+			item.innerHTML = this.lucy.sentences[i];
+			list.appendChild(item);
+		}
+		appendTo.appendChild(list);
+	}
+});
---
+++ Hello-Lucy-master/Hello-Lucy-master/readME.md
@@ -0,0 +1,146 @@
+## Hello-Lucy
+
+### Streamlined off-line voice control for MM
+
+### New and improved. More user friendly and more improvements to come.
+
+## Special thanks
+* sdetweil for unwavering support, instruction, error correction and amusing commentary.
+
+## Acknowledgements
+* cowboysdude for inspiration, modal column magic and audio support
+* STRAWBERRY 3.141 for MMM-voice
+* TheStigh for VoiceCommander
+
+## From the original Hello-Lucy
+* A short video of Hello-Lucy with Pages! https://youtu.be/bKHEXPzVb2A
+
+## Custom Audio support
+* The sounds you will have to create on your own. It's not that hard.
+
+## Add any module to work with Hello-Lucy!
+* Simplified process. See below.
+
+## Very important Pi users!
+* Read and perform the steps in the piAudioSetup.md file to set up your microphone.
+* Your microphone should be working BEFORE the installation.
+* Run `arecord -l` in your terminal. You need the card# and device# of your microphone/sound card for config entry.
+
+## 3 Step Installation
+### Please complete all 3
+
+* `git clone https://github.com/mykle1/Hello-Lucy` into the ~/MagicMirror/modules directory
+* `cd Hello-Lucy/installers`
+* `bash dependencies.sh`
+
+## Config options
+
+```
+{
+disabled: false,
+module: "Hello-Lucy",
+position: "top_center",
+config: {
+    keyword: 'HELLO LUCY',              // keyword to activate listening for a command/sentence
+    timeout: 15,                        // timeout listening for a command/sentence
+    standByMethod: 'DPMS',              // 'DPMS' = anything else than RPi or 'PI'
+    microphone: "0,0",                  // run "arecord -l" card # and device # mine is "0,0"
+    sounds: ["1.mp3", "11.mp3"],        // welcome sound at startup. Add several for a random greetings
+    confirmationSound: "ding.mp3",      // name and extension of sound file
+    startHideAll: true,                 // All modules start as hidden EXCEPT PAGE ONE
+    // *** Page One is your default startup page *** This overrides startHideAll: true,
+    pageOneModules: ["Hello-Lucy","MMM-EasyPix"],                     // default modules to show on page one/startup
+    pageTwoModules: ["Hello-Lucy", "MMM-BMW-DS", "MMM-EventHorizon"], // modules to show on page two
+    pageThreeModules: ["Hello-Lucy", "MMM-Lunartic"],                 // modules to show on page three
+    pageFourModules: ["Hello-Lucy", "MMM-PC-Stats"],                  // modules to show on page four
+    pageFiveModules: ["Hello-Lucy", "MMM-Searchlight"],               // modules to show on page five
+    pageSixModules: ["Hello-Lucy", "MMM-NOAA3"],                      // modules to show on page six
+    pageSevenModules: ["Hello-Lucy", "MMM-Recipe"],                   // modules to show on page seven
+    pageEightModules: ["Hello-Lucy", "MMM-rfacts"],                   // modules to show on page eight
+    pageNineModules: ["Hello-Lucy", "MMM-History"],                   // modules to show on page nine
+    pageTenModules: ["Hello-Lucy", "MMM-HardwareMonitor"]             // modules to show on page ten
+    }
+},
+```
+## StandBy options
+| **Option** | **Default** | **Description** | **info** |
+| --- | --- | --- | --- |
+| `standByMethod` | OPTIONAL | | |
+|        |          |'PI' |  use the tvservice command available on Raspberry pi to turn off the HDMI monitor source |
+|  |  | 'DPMS' |  use the exec DMPS command to turn off the monitor source (not on pi, or not hdmi) (default) |
+|  |  | 'HIDE' |  hide all module content, if display is on EnergyStar device that shows ugly 'no signal' screen for the other two choices |
+| `startHidden` | true/false | whether at MagciMirror startup to hide all modules and show image | default=true |
+
+## How to add any module you want for use with Hello-Lucy
+* Please read the "How to add modules to Hello-Lucy" file included with this repo.
+
+### Future upgrades for Hello_Lucy will eliminate the need for any modifications by the user (That's you)
+
+## Modules below that work by default
+
+* alert
+* clock
+* calendar
+* compliments
+* currentweather
+* newsfeed
+* weatherforecast
+*
+* Hello-Lucy
+* MMM-Advice
+* MMM-AVStock
+* MMM-AfterShip
+* MMM-Astro
+* MMM-ATM
+* MMM-BMW-CC (using ClimaCell)
+* MMM-BMW-DS (using Darksky)
+* MMM-BMW-OW (Using OpenWeather)
+* MMM-CalendarExt2
+* MMM-CARDS
+* MMM-Census
+* MMM-Cocktails
+* MMM-DailyQuotes
+* MMM-DarkSkyForecast
+* MMM-EARTH
+* MMM-EarthWinds
+* MMM-EasyBack
+* MMM-EasyPix
+* MMM-EventHorizon
+* MMM-Events
+* MMM-EveryNews
+* MMM-EyeCandy
+* MMM-FMI
+* MMM-Fortune
+* MMM-Gas
+* MMM-Glock
+* MMM-History
+* MMM-Holiday
+* MMM-ISS
+* MMM-ISS-Live
+* MMM-Insults
+* MMM-JEOPARDY
+* MMM-LICE
+* MMM-lichess-daily
+* MMM-Lottery
+* MMM-Lunartic
+* MMM-MARS
+* MMM-MercuryInRetrograde
+* MMM-MLB
+* MMM-MLBStandings
+* MMM-MusicCharts
+* MMM-NASA
+* MMM-NHL
+* MMM-NiceThings
+* MMM-NOAA3
+* MMM-PC-Stats
+* MMM-PetFinder
+* MMM-PilotWX
+* MMM-PLOW
+* MMM-Recipe
+* MMM-Remote-Control
+* MMM-rfacts
+* MMM-SORT
+* MMM-SoundMachine
+* MMM-SunRiseSet
+* MMM-ToDoLive
+* MMM-WeatherOrNot
